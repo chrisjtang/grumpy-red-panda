bd3c5bea5ee6e437f73bb1f0cdd3676d2c8bc3c3
Chris Tang <chrisjtang@gmail.com>
2023-06-13 12:57:00 -0700

    add bio

---
+++ src/Bio.tsx
@@ -0,0 +1,23 @@
+function Bio() {
+    return (
+      <>
+        <h1>Hello, world!</h1>
+        <div className='bio'>
+          <p>
+              My name is Chris and I'm a software engineer working on Zeta Global's <a href='https://disqus.com/' target='_blank'>Disqus</a> team. Disqus is the world’s largest engagement platform, and we serve an international network of 20 million commenters that leave 50 million comments and generate over 2.5 billion ad impressions per month.
+          </p>
+          <p>
+              Aside from Disqus, I have also worked at <a href='https://viahero.com/' target='_blank'>ViaHero</a> - a travel service that has connected 20k+ travelers with locals for 11k+ trips, <a href='https://github.com/open-source-labs/ReacType' target='_blank'>ReacType</a> - a prototyping tool with 1.9k+ Github stars, and UCLA's <a href='https://humtech.ucla.edu/' target='_blank'>Humanities Technology</a> - a unit that supports 16 departments consisting of 200 staff and 40k students at the University of California in Los Angeles.
+          </p>
+          <p>
+              In my engineering work, I strive to balance my technical background across the stack with an aptitude for project management that is focused on delivering meaningful results.  At Disqus, I led an international cross-functional team in rewriting the Disqus B2B website, finishing a month ahead of schedule and leading to a <strong>20% increase</strong> in inbound leads.  And at UCLA, I managed a team of 7 through a yearlong migration of over 10TB of business data for the entire Humanities division from a sunsetting physical server to cloud storage that resulted in over <strong>$150k savings annually</strong> in operational costs.
+          </p>
+          <p>
+              When I'm not at my computer, you can find me playing basketball, frequenting ethnic food joints in Los Angeles, traveling, or learning a new song on the keyboard.
+          </p>
+        </div>
+      </>
+    )
+  }
+  
+  export default Bio
